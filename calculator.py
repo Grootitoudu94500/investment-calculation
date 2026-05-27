a24b320ab63758360ee019b5c8c9b7f2be4784f9
Lesage Ulm Titouan <titouan.lesage-ulm@orange.fr>
2026-05-28 01:17:10 +0200

    Fix: Convert years to int for range() function

--- calculator.py
+++ calculator.py
@@ -1,11 +1,11 @@
 start = int(input("Starting number : "))
 percentage = float(input("Percentage number : "))
-years = float(input("Number of years : "))
+years = int(input("Number of years : "))
 def calculation(initial_amount, rate, duration):
   total = initial_amount
   for i in range(duration):
     total = total + (total * rate / 100)
-    pure_gain = total - initial_amount
+  pure_gain = total - initial_amount
   return pure_gain
 final_result = calculation(start, percentage, years)
 print(f"The gain is {final_result:.2f} or {final_result + start:.2f}")
